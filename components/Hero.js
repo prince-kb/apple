5578c889fb33d47de53c73c5c7748112148d250b
Prince <princejaj67@gmail.com>
2024-05-08 13:06:22 +0530

    Completed Highlights Section of the Page

--- components/Hero.js
+++ components/Hero.js
@@ -5,8 +5,10 @@ import Video from 'next-video'
 import { heroVideo,smallHeroVideo } from '@/utils';
 import { useEffect, useState } from 'react';
 const Hero = () => {
-    const [videoSrc, setVideoSrc] = useState(window.innerWidth > 760 ? heroVideo : smallHeroVideo)
+    // const [videoSrc, setVideoSrc] = useState(window.innerWidth > 760 ? heroVideo : smallHeroVideo)
+    const [videoSrc, setVideoSrc] = useState(heroVideo)
     useEffect(() => {
+        handleVideoSrcSet
         window.addEventListener('resize', handleVideoSrcSet)
         return () => {
             window.removeEventListener('resize', handleVideoSrcSet)
@@ -16,15 +18,15 @@ const Hero = () => {
         setVideoSrc(window.innerWidth > 760 ? heroVideo : smallHeroVideo);
     }
     useGSAP(()=>{
-        gsap.to('#hero',{opacity:1,duration:1.5})
-        gsap.to('#cta',{opacity:1,y:0,duration:1.5,delay:1.5})
+        gsap.to('#hero',{opacity:1,duration:2})
+        gsap.to('#cta',{opacity:1,y:0,duration:2,delay:1.5})
     },[])
   return (
-    <section className="w-full nav-height bg-black relative">
+    <section className="w-full nav-height relative">
         <div className="h-5/6 w-full flex-center flex-col">
             <p id="hero" className="hero-title"> iPhone 15 Pro</p>
-            <div className="md:w-10/12 w-9/12">
-                <Video src={videoSrc} autoPlay controls={false} muted playsInline={true} key={videoSrc}/>
+            <div className="md:w-10/12 w-9/12 overflow-hidden">
+                <Video src={videoSrc} autoPlay muted playsInline={true} controls={false} key={videoSrc} preLoad="none" className="z-[-1]"/>
             </div>
 
         </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <header className=" w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
         <Image src={appleImg} alt="Apple" width={14} height={18} />
-        <div className='flex flex-1 justify-center max-sm:hidden'>
+        <div className='flex flex-1 justify-center '>
             {navLists.map((item, i) => (
                 <div key={i} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>{item}</div>
             ))}
